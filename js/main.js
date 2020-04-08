a75412d788355369a7f26fa01afa129b9f9c5c8e
a.vodolazskikh <a.vodolazskikh@2gis.ru>
2020-04-08 16:04:36 +0700

    clear js part2

--- js/main.js
+++ js/main.js
@@ -1,72 +1,11 @@
 "use strict";
 
-const output = document.querySelector(".modal__value");
-const rangeSLider = document.querySelector(".adjust-bar.adjust-bar_theme_temp");
-
-// rangeSLider.oninput = function() {
-//     output.innerHTML = this.value > 0 ? '+' + this.value : this.value;
-// }
-
-const arrowLeftDevs = document.querySelector(
-  ".devices__paginator .paginator__arrow_left"
-);
-const arrowRightDevs = document.querySelector(
-  ".devices__paginator .paginator__arrow_right"
-);
-const panelCountDevs = document.querySelectorAll(".devices__panel").length;
-const devices = document.querySelector(".devices");
-const pagiantorDevs = document.querySelector(".devices__paginator");
-let currentPageDevs = 1;
-
-// pagiantorDevs.classList.toggle('paginator_hide', panelCountDevs < 7);
-
 $(".card").each(function (e) {
   if ($(this).hasClass("card_size_s")) {
     $(this).css({ "border-radius": "22px" });
   } else {
     $(this).css({ "border-radius": "54px" });
   }
-});
-
-document.querySelectorAll(".modal_close").forEach((b) => {
-  b.onclick = function () {
-    document.querySelectorAll(".modal").forEach((m) => {
-      m.classList.toggle("modal_open", false);
-      document.querySelector("body").style.overflow = "auto";
-    });
-  };
-});
-
-const TEMPS = {
-  manual: -10,
-  cold: 0,
-  warm: 23,
-  hot: 30,
-};
-
-document.querySelectorAll(".modal__filter-item_temp").forEach((l) => {
-  l.onclick = function () {
-    document.querySelector(".adjust-bar_theme_temp").value = TEMPS[this.id];
-    document.querySelector(".modal_temp .modal__value").innerHTML =
-      TEMPS[this.id] > 0 ? "+" + TEMPS[this.id] : TEMPS[this.id];
-  };
-});
-
-const showModal = function (selector) {
-  document.querySelector(selector).classList.toggle("modal_open", true);
-  document.querySelector("body").style.overflow = "hidden";
-};
-
-document.querySelectorAll(".panel_lamp").forEach((p) => {
-  p.onclick = function () {
-    showModal(".modal_light");
-  };
-});
-
-document.querySelectorAll(".panel_floor").forEach((p) => {
-  p.onclick = function () {
-    showModal(".modal_knob");
-  };
 });
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -90,43 +29,6 @@ document.addEventListener("DOMContentLoaded", function () {
     '<p class="card-source">Чайник</p>' +
     '<p class="card-time card-time_block">16:20, Сегодня</p>' +
     "</div>";
-});
-
-const arrowLeftScens = document.querySelector(
-  ".scenarios__paginator .paginator__arrow_left"
-);
-const arrowRightScens = document.querySelector(
-  ".scenarios__paginator .paginator__arrow_right"
-);
-const panelCountScens = document.querySelectorAll(".scenarios__panel").length;
-const pageCountScens = document.querySelectorAll(".scenarios__page").length;
-const scenarios = document.querySelector(".scenarios");
-const pagiantorScens = document.querySelector(".scenarios__paginator");
-let currentPage = 1;
-
-const selectButton = document.querySelector(".filter__select-button");
-const selectButtonText = document.querySelector(
-  ".filter__select-button .button__text"
-);
-const selectOptions = document.querySelectorAll(".filter__select-item");
-const popup = document.querySelector(".filter__select-popup");
-
-let widths = "";
-window.addEventListener("scroll", function () {
-  widths += document.querySelectorAll("body")[0].offsetWidth;
-});
-
-selectOptions.forEach((o) => {
-  o.addEventListener("click", function (e) {
-    document.querySelector("#" + e.target.dataset.group).checked = true;
-
-    selectOptions.forEach((opt) =>
-      opt.classList.toggle("filter__select-item_checked", false)
-    );
-    e.target.classList.toggle("filter__select-item_checked", true);
-    popup.classList.toggle("filter__select-popup_open", false);
-    selectButtonText.innerText = e.target.innerText;
-  });
 });
 
 $(document).ready(function () {
