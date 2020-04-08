b9c1a5963e0bd11d8ebe5eee20cad3a090daed56
a.vodolazskikh <a.vodolazskikh@2gis.ru>
2020-04-08 20:58:08 +0700

    delete multimedia.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", () => {
-  const multimediaScript = document.createElement("script");
-  multimediaScript.setAttribute("src", "./js/multimedia.js");
   const buttonsContainer = document.querySelector(".buttons-wrap");
-
   buttonsContainer.innerHTML =
     '<button class="button button_yellow" type="button">Да</button>' +
     '<button class="button">Нет</button>';
-
-  document.querySelector(".multimedia").appendChild(multimediaScript);
 });
 
 $(document).ready(function () {
